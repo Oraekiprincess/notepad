16dc61b302fbcc65d3c811b2a54f93159e28c437
Oraekiprincess <114993346+Oraekiprincess@users.noreply.github.com>
2023-02-27 17:16:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,70 @@
-import React, { useState } from "react";
-import './App.css';
+/*import './App.css';
 
 function App() {
-
-
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
-
-  const addTodo =  (todo) => {
-    const newTodo = {
-      id: Math.random(),
-      todo: todo,
-    };
-
-    setList([...list, newTodo]);
-
-    setInput("");
-  };
-
-  const deleteTodo = (id) => {
-
-      const newList = list.filter((todo) => todo.id !==id);
-
-    setList(newList);
-  };
-
   return (
-    <div className="Input">
-    <div>
-<h1 className="head">Todo List</h1>
-<input className="change" 
-type="text"
- value={input}
- onChange={(e) => setInput(e.target.value)}
-/>
-<button className="btn" onClick={() => addTodo(input)}>Add</button>
-<ul className="ulist">
-  {list.map((todo) => (
-    <li className="list" key={todo.id}>
-      {todo.todo}
-      <button className="button" onClick={() => deleteTodo(todo.id)}>Delete</button>
-    </li>
-    ))}</ul>
-  </div>
-  </div>
+    <div className="App">
+      
+    </div>
   );
 }
+
+export default App;*/
+
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+import Search from './components/Search';
+ 
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '25/02/2023',
+    },
+    {
+        id: nanoid(),
+        text: 'This is my second note',
+        date: '25/02/2023',
+    },
+    {
+        id: nanoid(),
+        text: 'This is my third one',
+        date: '25/02/2023',
+    },
+    {
+      id: nanoid(),
+      text: 'new one',
+      date: '25/02/2023',
+    },
+  ]);
+
+  const addNote = (text) => {
+    const  date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+  
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note)=>note.id !== id);
+    setNotes(newNotes);
+  }
+
+    return (
+        <div className='container'>
+              <Search />
+              <NotesList
+                  notes={notes}
+                  handleAddNote={addNote} 
+                  handleDeleteNote={deleteNote}
+              />
+        </div>
+    );
+  };
 
 export default App;
